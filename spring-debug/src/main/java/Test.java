c314e41e0e0b251f7e23dab8b4c15e7d767394b4
zining <daveafei@hotmail.com>
2023-08-29 18:19:52 +0800

    init

--- spring-debug/src/main/java/Test.java
+++ spring-debug/src/main/java/Test.java
@@ -1,6 +1,15 @@
+import org.springframework.AService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Miho");
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("tx.xml");
+		System.out.println("你好");
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("tx.xml");
+		MyClassApplicationContext applicationContext = new MyClassApplicationContext("tx.xml");
+		System.out.println(applicationContext);
+		AService bean = applicationContext.getBean(AService.class);
+		System.out.println(bean.getName());
 	}
 }

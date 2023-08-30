269b0241f537eac60896cb743eada9fc5576f57e
zining <daveafei@hotmail.com>
2023-08-30 16:31:36 +0800

    customize element is added

--- spring-debug/src/main/java/Test.java
+++ spring-debug/src/main/java/Test.java
@@ -7,13 +7,6 @@ import java.lang.reflect.Field;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
-		Class<?> aClass = Class.forName("org.flagtag.User");
-		Object o = aClass.newInstance();
-		Field[] fields = aClass.getFields();
-		Field userName = aClass.getField("userName");
-		userName.set(o,"zhangsan");
-		System.out.println(o);
-		System.out.println("Miho");
 		System.out.println("你好");
 //        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("tx.xml");
 		MyClassApplicationContext applicationContext = new MyClassApplicationContext("editor.xml");
